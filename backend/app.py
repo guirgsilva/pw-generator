02599b39708ed4725eb3db87dfad8884c85da5ea
guirgsilva <48664751+guirgsilva@users.noreply.github.com>
2024-08-24 11:51:55 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,14 +8,12 @@ CORS(app)
 
 @app.route('/generate-password', methods=['POST'])
 def generate_password():
-    # Get parameters from the request
     length = request.json.get('length', 12)
     use_uppercase = request.json.get('useUppercase', True)
     use_lowercase = request.json.get('useLowercase', True)
     use_numbers = request.json.get('useNumbers', True)
     use_symbols = request.json.get('useSymbols', True)
 
-    # Define the character set based on user preferences
     characters = ''
     if use_uppercase:
         characters += string.ascii_uppercase
@@ -26,13 +24,11 @@ def generate_password():
     if use_symbols:
         characters += string.punctuation
 
-    # Check if at least one character type is selected
     if not characters:
         return jsonify({"error": "At least one character type must be selected"}), 400
 
-    # Generate the password
     password = ''.join(random.choice(characters) for _ in range(length))
     return jsonify({"password": password})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True)
